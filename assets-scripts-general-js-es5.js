662b67bf929c726d2350d68049cb1df3ec810bd7
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2021-02-17 06:38:14 -0200

    Auto-generated commit

---
+++ assets-scripts-general-js-es5.js
@@ -0,0 +1,21 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["assets-scripts-general-js"],{
+
+/***/ "./src/assets/scripts/general.js":
+/*!***************************************!*\
+  !*** ./src/assets/scripts/general.js ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+$(document).ready(function() {
+   $(".panel-left").resizable({
+      handleSelector: ".splitter",
+      resizeHeight: false
+   });
+});
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=assets-scripts-general-js-es5.js.map
