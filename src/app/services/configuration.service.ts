1540252b1f07da7f22e29430a153fef8eee914d3
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2021-03-17 12:53:55 -0300

    Adicionando o url do servidor dos cubos

--- src/app/services/configuration.service.ts
+++ src/app/services/configuration.service.ts
@@ -2,15 +2,11 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ConfigurationService {
-   // public Server = 'http://combaftp.inf.ufrgs.br:10091/';
-   // public Server = 'http://143.54.13.50:7000/';
-   public Server = 'http://127.0.0.1:7000/';
-   // public Server = 'http://34.204.217.23:7000/';
+   public Server = 'http://saude.inf.ufrgs.br:7000/'
    public ApiUrl = 'api';
    public ServerWithApiUrl = this.Server + this.ApiUrl;
 
    public defaultDataset = 'qdscovid_registers';
-   //public defaultDataset = 'qdscovid_registros';
 
    constructor() { }
 
